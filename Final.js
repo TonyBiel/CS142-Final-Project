60881c426cc02d6f7325732589d9561e84c47a93
TonyBiel <abiel142002@gmail.com>
2026-04-29 15:54:07 -0500

    Initialize Final.js with game variables and constants
    added global variables

---
+++ Final.js
@@ -0,0 +1,39 @@
+// canvas variables
+var canvas, pen;
+
+// game constants
+var bubbleRadius = 10;
+var bubbleSpeed = 2;
+var totalBubbles = 100;
+
+// game state
+var bubbles;
+var stepCount;
+var burstCount;
+var escapedCount;
+var releasedCount;
+var releaseRate;
+var timer;
+var gameOver;
+
+// baton variables
+var batonX;
+var batonY;
+var batonWidth;
+var batonHeight;
+var batonDX;
+var batonSpeed;
+var batonColor;
+
+
+
+
+
+
+
+
+if easy
+
+else if moderate
+
+else hard 
